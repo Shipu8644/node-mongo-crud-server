96213db3a82c3b280a3e187f73d10b823bc5d585
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-23 00:15:17 +0600

    done crud

--- index.js
+++ index.js
@@ -1,14 +1,97 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const objectId = require('mongodb').ObjectId;
+
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient } = require('mongodb');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://mydbuser1:Avfyo1v7UkVrpm2s@cluster0.om5y9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // user: mydbuser1
 // password: Avfyo1v7UkVrpm2s
 
 
 
-const uri = "mongodb+srv://mydbuser1:Avfyo1v7UkVrpm2s@cluster0.om5y9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("foodMaster");
+        const usersCollection = database.collection("users");
+
+        // get Api
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: objectId(id) };
+            const user = await usersCollection.findOne(query);
+            // console.log('load user with id: ', id);
+            res.send(user);
+        })
+
+
+        //   POST API
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            console.log('got new user', req.body);
+            console.log('added user', result);
+            res.send(result);
+        })
+        //UPDATE API
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = { _id: objectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    email: updatedUser.email
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options)
+            console.log('updating', id)
+            res.json(result)
+        })
+        // delete Api
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: objectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            console.log('deleting users', id);
+            console.log('deleting user with id', result);
+            res.json(result)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello from Mongodb crud');
+})
+
+app.listen(port, () => {
+    console.log('Running server on port', port);
+})
+
+
+
 
 // client.connect(err => {
 //     const collection = client.db("foodMaster").collection("users");
@@ -21,29 +104,3 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //         })
 //     // client.close();
 // });
-
-async function run() {
-    try {
-        await client.connect();
-        const database = client.db("foodMaster");
-        const usersCollection = database.collection("users");
-        // create a document to insert
-        const doc = {
-            name: "special 1",
-            email: "special@gmail.com",
-        }
-        const result = await usersCollection.insertOne(doc);
-        console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    } finally {
-        await client.close();
-    }
-}
-run().catch(console.dir);
-
-app.get('/', (req, res) => {
-    res.send('Hello from Mongodb crud');
-})
-
-app.listen(port, () => {
-    console.log('Running server on port', port);
-})
